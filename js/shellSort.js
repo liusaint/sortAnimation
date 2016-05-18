07e7e19c650ffbf19a2e90683d37a553aaa047a2
Runner <841766635@qq.com>
2016-05-18 22:11:31 +0800

    希尔排序修改。
    1.去jQuery。
    2.优化DOM操作。
    3.不在原数组上操作。

--- js/shellSort.js
+++ js/shellSort.js
@@ -1,31 +1,45 @@
 
+/*
+*  arr:数组
+*  a:当前本轮已完成排序到哪一位。排序循环中的i。
+*  b:当前在比较的位。循环中的j;
+*  c:标记本轮插入排序的第一位。循环中的k。
+*  temp:当前找到的最小值;
+*  gap:当前排序的间隔;
+*/
+
+
+
 function shellSortDom(arr,a,b,c,temp,gap){
-	$(".sort_ul").empty();
-	var html='',item= '';
-	var len = arr.length;
-	$.each(arr, function(i, val) {
-		item = '<li class="sort_li"><span class="sort_span" style="height: '+val+'%"></span></li>';
+
+	var html='',item= '',len = arr.length,i;
+
+	for (i = 0; i < len; i++) {
+		spanClass = 'sort_span';
+
+		if(c<=i &&((i-c)%gap == 0)){
+			spanClass += ' sort_span_blue';
+		}
+
+		if(c<=i && i<= a && ((i-c)%gap == 0)){
+			spanClass += ' sort_span_green';
+		}
+
+		if(i == b){
+			item = 	'<li class="sort_li"><span class="'+ spanClass +'" style="height: '+arr[i]+'%"></span><span class="sort_span_in" style="height:'+temp+'%"></span></li>';
+		}else{
+			item = '<li class="sort_li"><span class="'+ spanClass +'" style="height: '+arr[i]+'%"></span></li>';
+		}
 		html= html+item;
-	});
-	$(".sort_ul").append(html);
-
-	for (var i = c; i < len; i=i+gap) {
-		$(".sort_span").eq(i).addClass('sort_span_blue');
 	}
 
-
-	for (var i = c; i <= a; i=i+gap) {
-		$(".sort_span").eq(i).addClass('sort_span_green');
-	}
-
-	
-	$(".sort_span").eq(b).parent().append('<span class="sort_span_in" style="height:'+temp+'%"></span>');
+	document.querySelector('.sort_ul').innerHTML = html;
 }
 
 
 
-function shellSort(arr){
-	var i,k,j,len=arr.length,gap = Math.ceil(len/2),temp,timeCount=0;
+function shellSort(oriArr){
+	var arr =oriArr.slice(),i,k,j,len=arr.length,gap = Math.ceil(len/2),temp;
 	while(gap>0){
 		for (var k = 0; k < gap; k++) {
 			for (i = k+gap; i < len; i=i+gap) {				
